c3e01634460e5a4103c93a0160607d106fdde034
jamezaguiar <jamersonaguiar@alu.ufc.br>
2019-10-14 17:21:43 -0300

    questao04.sh fix

--- prova02/questao04.sh
+++ prova02/questao04.sh
@@ -2,44 +2,39 @@
 multTable() {
   num=$1
   clear
-  distanceX=1
-  distanceY=1
+  distanceX=2
+  distanceY=4
+  tput setaf 1
+  tput bold
+  for x in $(seq 1 $num); do
+    tput cup 0 $distanceY
+    echo $x
+    tput cup $distanceX 0
+    echo $x
+    distanceX=$(expr $distanceX + 2)
+    distanceY=$(expr $distanceY + 4)
+  done
+  tput sgr0
+  distanceX=2
+  distanceY=4
+
   for coluna in $(seq 1 $num); do
     for linha in $(seq 1 $num); do
       tput sgr0
       if [ "$linha" -eq "$coluna" ]; then
         tput smul
+      fi
+      mult=$(expr $linha \* $coluna)
+      tput cup $distanceX $distanceY
+      echo $mult
+      distanceX=$(expr $distanceX + 2)
+      if [ "$linha" -eq "$coluna" ]; then
+        tput smul
       else
         tput sgr0
       fi
-      if [ "$coluna" -eq 1 ]; then
-        tput bold
-        tput setaf 1
-        mult=$(expr $linha \* $coluna)
-        tput cup $distanceX $distanceY
-        echo $mult
-        distanceX=$(expr $distanceX + 2)
-      else
-        if [ "$linha" -eq 1 ]; then
-          tput bold
-          tput setaf 1
-        else
-          if [ "$linha" -eq "$coluna" ]; then
-            tput smul
-          else
-            tput sgr0
-          fi
-        fi
-        mult=$(expr $linha \* $coluna)
-        tput cup $distanceX $distanceY
-        echo $mult
-        distanceX=$(expr $distanceX + 2)
-
-      #else
-      #tput rmul
-      fi
     done
-    distanceX=1
+    distanceX=2
     distanceY=$(expr $distanceY + 4)
   done
 }
